6a4519780071bcb7029c6f28d765ee2aab6887e1
CarlosPrieto <87465747+CarlosPrieto66@users.noreply.github.com>
2022-01-23 22:43:13 -0800

    animation

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-let menuBtn = document.querySelector('.menu-button')
-let dropdownNav = document.querySelector('.dropdownNav')
+// let menuBtn = document.querySelector('.menu-button')
+// let dropdownNav = document.querySelector('.dropdownNav')
 
-menuBtn.addEventListener('click', (e) => {
+// menuBtn.addEventListener('click', (e) => {
 
-    if (dropdownNav.classList.contains('closed')) {
-    dropdownNav.classList.remove('closed')
-    document.getElementById("menuIcon").src="images/Logo-opened-64px.png";
-    document.getElementById("navBar").style.backgroundColor = "rgba(0, 0, 0, 0.95)";
+//     if (dropdownNav.classList.contains('closed')) {
+//     dropdownNav.classList.remove('closed')
+//     document.getElementById("menuIcon").src="images/Logo-opened-64px.png";
+//     document.getElementById("navBar").style.backgroundColor = "rgba(0, 0, 0, 0.95)";
   
-  } else {
-    dropdownNav.classList.add('closed')  
-    document.getElementById("menuIcon").src="images/Logo-closed-64px.png"; 
-    document.getElementById("navBar").style.backgroundColor = "rgba(0, 0, 0, 0.75)";
+//   } else {
+//     dropdownNav.classList.add('closed')  
+//     document.getElementById("menuIcon").src="images/Logo-closed-64px.png"; 
+//     document.getElementById("navBar").style.backgroundColor = "rgba(0, 0, 0, 0.75)";
 
 
-  }
+//   }
+// })
+
+
+
+$('.menu-toggle').click(function() {
+
+  $('.navbar').toggleClass('navbar-open', 500);
+  $('.nav-dropdown').toggleClass('nav-dropdown-open', 500);
+
+  $(this).toggleClass('menu-open');
+
 })
-
-// $(".menu-button").on("click", function(){
-//   // $(".toggleMe").toggle();
-//    console.log("clickedButton")
-//   $(".dropdownNav").slideToggle();
-// });
